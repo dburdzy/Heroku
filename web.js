99343fc9b57b960efb71e9f76806b66eb8749073
dburdzy <dburdzy@mywebgrocer.com>
2014-07-11 10:50:15 -0400

    Update web.js

--- web.js
+++ web.js
@@ -6,7 +6,13 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  var text = '2302095hlolaksdf';
+  
+var text = '{ "Recipes" : [' +
+'{ "Name":"John" , "Description":"Doe" , "Img_URL: www.nope.com" , "Servings : "1782" },' +
+'{ "Name":"John2" , "Description":"Doe2" , "Img_URL: www.nope2.com" , "Servings : "1783" },' +
+'{ "Name":"John3" , "Description":"Doe3" , "Img_URL: www.nope3.com" , "Servings : "1784" },' +
+'{ "Name":"John4" , "Description":"Doe4" , "Img_URL: www.nope4.com" , "Servings : "1888" } ]}';
+
   res.send(text);
 });
 
