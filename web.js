f6b75809575826fbc7102b84075acd5f66151a43
dburdzy <dburdzy@mywebgrocer.com>
2014-07-11 11:44:05 -0400

    Update web.js

--- web.js
+++ web.js
@@ -9,24 +9,7 @@ app.use(express.static(process.cwd() + '/info'));
 
 app.get('/', function(req, res) {
   
-<<<<<<< HEAD
-var text = '{ "Recipes" : [' +
-<<<<<<< HEAD
-'{ "Name":"John" , "Description":"Doe" , "Img_URL: www.nope.com" , "Servings : "1782" },' +
-'{ "Name":"John2" , "Description":"Doe2" , "Img_URL: www.nope2.com" , "Servings : "1783" },' +
-'{ "Name":"John3" , "Description":"Doe3" , "Img_URL: www.nope3.com" , "Servings : "1784" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-
-=======
-'{ "Name":"John1" , "Description":"Doe1" , "Img_URL": "www.nope1.com" , "Servings" : "1782" },' +
-'{ "Name":"John2" , "Description":"Doe2" , "Img_URL": "www.nope2.com" , "Servings" : "1783" },' +
-'{ "Name":"John3" , "Description":"Doe3" , "Img_URL": "www.nope3.com" , "Servings" : "1784" },' +
-'{ "Name":"John4" , "Description":"Doe4" , "Img_URL": "www.nope4.com" , "Servings" : "1888" } ]}';
->>>>>>> 9392bc5756d1aee658cb1c113a185c86c2933365
-
-=======
 var text = 'Hello Sir';
->>>>>>> 81f47d6cce7a17f5cddc280a95c15efa3d40bb76
   res.send(text);
 });
 
